a18d18d71433e597ba336a41d5b70f8207de9e0b
reissondesigner-lang <reissondesigner@gmail.com>
2026-02-12 14:55:31 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,42 @@
-const CACHE_NAME = "parcelamento-v1";
+const CACHE_NAME = "parcelamento-v2";
+
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "taxas.js",
+  "produtos.js"
+];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json"
-      ]);
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
     })
   );
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
