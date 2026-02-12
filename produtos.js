64606a9638ec599628eed7d71bd711d6eba0f79f
reissondesigner-lang <reissondesigner@gmail.com>
2026-02-12 13:27:17 -0300

    Add PRODUTOS array with product details

---
+++ produtos.js
@@ -0,0 +1,17 @@
+const PRODUTOS = [
+  {
+    nome:"Colchão Premium",
+    descricao:"Espuma alta densidade",
+    valor:3048
+  },
+  {
+    nome:"Colchão Luxo",
+    descricao:"Molas ensacadas",
+    valor:4590
+  },
+  {
+    nome:"Base Box",
+    descricao:"Estrutura reforçada",
+    valor:890
+  }
+];
